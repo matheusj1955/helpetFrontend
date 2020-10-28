f05bc0d2d8e43b0bf2d9433e3f269feabc8cbf03
Matheus J <matheus.pro1995@gmail.com>
2020-10-27 21:09:04 -0300

    cadastro de usuario esta funcionado e a validação tbm

--- lib/casdastro.dart
+++ lib/casdastro.dart
@@ -14,6 +14,7 @@ class Cadastro extends StatefulWidget {
 class _LoginState extends State<Cadastro> {
 
   ServiceUsuario serviceUsuario = ServiceUsuario();
+  final _formKey = GlobalKey<FormState>();
 
   final nomeField = TextEditingController();
   final emailField = TextEditingController();
@@ -34,132 +35,184 @@ class _LoginState extends State<Cadastro> {
           left: 40,
         ),
         color: Colors.white,
-        child: ListView(
-          children: <Widget>[
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
 //            _nomeCadastro =
-            TextFormField(
-              controller: nomeField,
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Nome",
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(fontSize: 20),
-            ),
-            TextFormField(
-              controller: telefoneField,
-              keyboardType: TextInputType.phone,
-              decoration: InputDecoration(
-                labelText: "Telefone",
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              controller: emailField,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  height: 40,
-                  width: 135,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.topRight,
-                        stops: [0.3, 1],
-                        colors: [
-                          Color(0xffa56336),
-                          Colors.amber,
-                        ],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                  child: FlatButton(
-                    onPressed: () {
-                      Usuario usuario = Usuario(null, nomeField.text, telefoneField.text, emailField.text);
-                      serviceUsuario.PostUsuario(usuario);
-                    },
-                    child: Text(
-                      "Cadastrar",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
+              TextFormField(
+                validator: _validarNome,
+                controller: nomeField,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: "Nome",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
                   ),
                 ),
-                SizedBox(
-                  width: 5,
+                style: TextStyle(fontSize: 20),
+              ),
+              TextFormField(
+                validator: _validarCelular,
+                controller: telefoneField,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  labelText: "Telefone",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
                 ),
-                Container(
-                  height: 40,
-                  width: 135,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topLeft,
-                        end: Alignment.topRight,
-                        stops: [0.3, 1],
-                        colors: [
-                          Color(0xffa56336),
-                          Colors.amber,
-                        ],
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(5))),
-                  child: FlatButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => Posts()));
-                    },
-                    child: Text("Cancelar",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                validator: _validarEmail,
+                controller: emailField,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                  ),
+                ),
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    height: 40,
+                    width: 135,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.topRight,
+                          stops: [0.3, 1],
+                          colors: [
+                            Color(0xffa56336),
+                            Colors.amber,
+                          ],
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: FlatButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Usuario usuario = Usuario(null, nomeField.text, telefoneField.text, emailField.text);
+                          serviceUsuario.PostUsuario(usuario);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) => Posts()));
+                        }
+                      },
+                      child: Text(
+                        "Cadastrar",
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 18,
-                        )),
+                        ),
+                      ),
+                    ),
                   ),
-                )
-              ],
-            ),
-          ],
-        ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Container(
+                    height: 40,
+                    width: 135,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.topRight,
+                          stops: [0.3, 1],
+                          colors: [
+                            Color(0xffa56336),
+                            Colors.amber,
+                          ],
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: FlatButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) => Posts()));
+                      },
+                      child: Text("Cancelar",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          )),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        )
+
       ),
     );
+  }
+}
+
+
+String _validarNome(String value) {
+  String patttern = r'(^[a-zA-Z ]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value.length == 0) {
+    return "Informe o nome";
+  } else if (!regExp.hasMatch(value)) {
+    return "O nome deve conter caracteres de a-z ou A-Z";
+  }
+  return null;
+}
+
+String _validarCelular(String value) {
+  String patttern = r'(^[0-9]*$)';
+  RegExp regExp = new RegExp(patttern);
+  if (value.length == 0) {
+    return "Informe o celular";
+  } else if(value.length != 10){
+    return "O celular deve ter 10 dígitos";
+  }else if (!regExp.hasMatch(value)) {
+    return "O número do celular so deve conter dígitos";
+  }
+  return null;
+}
+
+
+String _validarEmail(String value) {
+  String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@'
+      r'((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regExp = new RegExp(pattern);
+  if (value.length == 0) {
+    return "Informe o Email";
+  } else if(!regExp.hasMatch(value)){
+    return "Email inválido";
+  }else {
+    return null;
   }
 }
